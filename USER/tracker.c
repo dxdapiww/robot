1b88d900db439399ed875965c8a8f041e65923c6
jokertin <jokertin233@outlook.com>
2023-04-17 23:24:00 +0800

    first commit

---
+++ USER/tracker.c
@@ -0,0 +1,97 @@
+#include "timer.h"
+#include "led.h"
+#include "usart.h"
+#include "tracker.h"
+void Lane_Counter_Fwd_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC | RCC_APB2Periph_GPIOB |
+							   RCC_APB2Periph_GPIOA | RCC_APB2Periph_GPIOE,
+						   ENABLE); // 使能PORTA,PORTE时钟
+	// XJ1 0
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;	  
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU; 
+	GPIO_Init(GPIOA, &GPIO_InitStructure);		  
+
+	// XJ1 1
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOA, &GPIO_InitStructure);		 
+
+	// XJ1 2
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_6;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOA, &GPIO_InitStructure);		 
+
+	// XJ1 3
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOA, &GPIO_InitStructure);		  
+
+	// XJ1 4
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_4;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOC, &GPIO_InitStructure);		  
+
+	// XJ1 5
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOC, &GPIO_InitStructure);		 
+
+	// XJ1 6
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD;
+	GPIO_Init(GPIOB, &GPIO_InitStructure);		  
+
+	// XJ1 7
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOB, &GPIO_InitStructure);		  
+}
+
+void Lane_Coutner_Back_Init(void)
+{
+	GPIO_InitTypeDef GPIO_InitStructure;
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOF|RCC_APB2Periph_GPIOG,ENABLE); // 使能PORTA,PORTE时钟
+
+	//XJ2 0
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // PA0设置成输入，默认下拉
+	GPIO_Init(GPIOF, &GPIO_InitStructure);		  // 初始化GPIOA.0
+
+	//XJ2 1
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_14;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; // PA0设置成输入，默认下拉
+	GPIO_Init(GPIOF, &GPIO_InitStructure);		  // 初始化GPIOA.0
+
+	//XJ2 2
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOB, &GPIO_InitStructure);	
+
+	//XJ2 3
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOG, &GPIO_InitStructure);	
+
+	//XJ2 4
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_1;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOG, &GPIO_InitStructure);	
+
+	//XJ2 5
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_7;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOE, &GPIO_InitStructure);	
+
+	//XJ2 6
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOF, &GPIO_InitStructure);	
+
+	//XJ2 7
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_12;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPD; 
+	GPIO_Init(GPIOF, &GPIO_InitStructure);	
+
---
+++ USER/tracker.h
@@ -0,0 +1,7 @@
+#include "timer.h"
+#include "led.h"
+#include "usart.h"
+#include "stm32f10x.h"
+
+void Lane_Counter_Fwd_Init(void); //??????
+void Lane_Coutner_Back_Init(void);//??????
